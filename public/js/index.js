e532655f20c156cf8b6888684ddc6740b10aef3f
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-10-29 21:23:29 -0400

    change restart button

--- public/js/index.js
+++ public/js/index.js
@@ -130,194 +130,142 @@
   ];
   const form = document.forms[0];
   var replay = document.getElementById('restart');
-  function giveHint() {
-    //var random = Math.floor(Math.random() * (31 - 0) + 0);
-    var hintbox = document.getElementById('hint-box');
-    hintbox.textContent = teamNames[random].hintOne;
-
-  }
-
-
+  //var replayLose = document.getElementById('restart-lose')
   var letters = document.getElementById('letters');
   const guess = form.guess.value;
-  //var a = document.getElementById('letter-a');
-  //var b = document.getElementById('letter-b');
-  //var c = document.getElementById('letter-c');
-  //var d = document.getElementById('letter-d');
-  //var e = document.getElementById('letter-e');
-  //var f = document.getElementById('letter-f');
-  //var g = document.getElementById('letter-g');
-  //var h = document.getElementById('letter-h');
-  //var i = document.getElementById('letter-i');
-  //var j = document.getElementById('letter-j');
-  //var k = document.getElementById('letter-k');
-  //var l = document.getElementById('letter-l');
-  //var m = document.getElementById('letter-m');
-  //var n = document.getElementById('letter-n');
-  //var o = document.getElementById('letter-o');
-  //var p = document.getElementById('letter-p');
-  //var q = document.getElementById('letter-q');
-  //var r = document.getElementById('letter-r');
-  //var s = document.getElementById('letter-s');
-  //var t = document.getElementById('letter-t');
-  //var u = document.getElementById('letter-u');
-  //var v = document.getElementById('letter-v');
-  //var w = document.getElementById('letter-w');
-  //var x = document.getElementById('letter-x');
-  //var y = document.getElementById('letter-y');
-  //var z = document.getElementById('letter-z');
+  var hintbox = document.getElementById('hint-box');
+  var numTries = document.getElementById('tries');
+//let answer, random, state;
   //var random = Math.floor(Math.random() * (31 - 0) + 0);
-  //var random = Math.floor(Math.random() * (31 - 0) + 0);
-//let random, state, answer;
+  //console.log(random);
+  var random = Math.floor(Math.random() * (31 - 0) + 0);
+  var answer = teamNames[random].name;
+  var state = answer.split("").fill("_");
+      var triesCounter = numTries.textContent;
+
+
+      letters.textContent = state.join(" ");
+      hintbox.textContent = "";
+      numTries.textContent = 0;
+//var random, state, answer;
   var hintbox = document.getElementById('hint-box');
   var numTries = document.getElementById('tries');
   //const random = Math.floor(Math.random() * (31 - 0) + 0);
 //console.log(random);
 
-  //const answer = teamNames[random].name;
+  var answer = teamNames[random].name;
   var triesCounter = numTries.textContent;
-  var random = Math.floor(Math.random() * (31 - 0) + 0);
-  var state = teamNames[random].name.split("").fill('_');
+  //const random = Math.floor(Math.random() * (31 - 0) + 0);
   //console.log(random);
- var answer = teamNames[random].name;
   //console.log(random);
 //let random, state, answer, teanNames;
 
+function getRandomNumber() {
+var random =  Math.floor(Math.random() * (31 - 0) + 0);
+return random;
+};
+
+//getRandomNumber();
 
 
 
-
-  //var answer = teamNames[x].name;
-  //let state = answer.split("").fill("_");
   //word.replace(/[abcdefghijklmnopqrstuvwxyz]/ig, "_");
 
 
   function restartGame() {
-random = "";
+    //const random = Math.floor(Math.random() * (31 - 0) + 0);
     //var random = Math.floor(Math.random() * (31 - 0) + 0);
+//getRandomNumber();
+var random = null;
     console.log(random);
-    let hintBox = "";
-    let state = "";
-    let answer = "";
+//var hintBox= "";
+//hintBox.textContent = null;
+hint.removeEventListener('click', giveHint);
+form.removeEventListener('submit', guess);
+
+
+    var state = "";
+    var answer = "";
+    //var guess = null;
 
     startGame();
 
 };
 
+//START FUNCTION
+
   function startGame() {
   //random = "";
-
-    var random = Math.floor(Math.random() * (31 - 0) + 0);
+   //getRandomNumber();
+    var random = (Math.floor(Math.random() * (31 - 0) + 0));
     console.log(random);
-    var hintbox = document.getElementById('hint-box');
-    var numTries = document.getElementById('tries');
+
     var state = teamNames[random].name.split("").fill('_');
     console.log(random);
+
     var answer = teamNames[random].name;
     console.log(random);
+    var replay = document.getElementsByClassName('restart');
+
     var triesCounter = numTries.textContent;
 
 
     letters.textContent = state.join(" ");
     hintbox.textContent = "";
-    numTries.textContent = 5;
-    document.getElementById("restart").classList.add('hidden');
+    numTries.textContent = 0;
     document.getElementById("result-box").classList.add("hidden");
-  //}
+    document.getElementById("lose-box").classList.add("hidden");
+    hint.addEventListener('click', giveHint);
 
-  //function restart() {
-  //  var random = Math.floor(Math.random() * (31 - 0) + 0);
+    //function giveHint() {
+    //var random = Math.floor(Math.random() * (31 - 0) + 0);
 
-  //  var hintbox = document.getElementById('hint-box');
-  //  var numTries = document.getElementById('tries');
-  //  var state = teamNames[random].name.split("").fill('_');
-  //  var answer = teamNames[random].name;
-  //  var triesCounter = numTries.textContent;
-
-    //var answer = teamNames[random].name;
-  //  letters.textContent = state.join(" ");
-  //  hintbox.textContent = "";
-  //  numTries.textContent = 5;
-  //}
-}
-  function giveHint() {
-    var hintbox = document.getElementById('hint-box');
-    hintbox.textContent = teamNames[random].hintOne;
-    console.log(random);
-  }
+    //  var hintbox = document.getElementById('hint-box');
+  //    hintbox.textContent = teamNames[random].hintOne;
+  //  console.log(random);
+    }
 
 
 
 
 
 
+//  };
 
-//function validateGuess(guess) {
 
- //return guess && guess === /[abcdefghijklmnopqrstuvwxyz]/ig;
 
+//  function giveHint() {
+//    var random = Math.floor(Math.random() * (31 - 0) + 0);
+//    var hintbox = document.getElementById('hint-box');
+//    hintbox.textContent = teamNames[random].hintOne;
+//    console.log(random);
 //  }
 
 
+function giveHint() {
+//var random = Math.floor(Math.random() * (31 - 0) + 0);
 
-
-  //function takeGuess(guess) {
-    //var random = Math.floor(Math.random() * (31 - 0) + 0);
-  //  var state = teamNames[random].name.split("").fill('_');
-    //var answer = teamNames[random].name;
-
-    //var numTries = document.getElementById('tries');
-    //var replay = document.querySelector('.restart');
-
-    //let guesses = "";
-    //let answer = Array.from(teamNames[random].name);
-
-    //console.log(answer);
-    //console.log(state);
-
-    //for ( let i = 0; i < state.length; i++ ) {
-    //guess.toUpperCase();
-    //let letter = answer[i];
-    // if ( letter === guess ) {
-    //  state[i] = guess;
-    //letters.textContent = state.join(" ");
-    //}
-
-
-    //numTries.textContent--;
-  //}
+  var hintbox = document.getElementById('hint-box');
+  hintbox.textContent = teamNames[random].hintOne;
+console.log(random);
+}
 
 
 
-  //window.alert("You Lost! Try again!")
-  //  function loseGame() {
-  //    if (numTries.textContent <= 0) {
-  //      console.log(loser);
-  //    }
-  //  }
-
-
-
-
-
-  // };
-  //function loseGame() {
-  //  if ( numTries.textContent <= 0 ) {
-  //  alert("You Lost! Try again!")
-  //  }
-  //}
-
-  startGame();
+//window.addEventListener("onload", startGame());
   //loseGame();
-  form.addEventListener('submit', function(event) {
+  form.addEventListener('submit', function guess(event) {
     event.preventDefault();
 const guess = form.guess.value;
 
 
+
+
 //if (guess === includes(/[abcdefghijklmnopqrstuvwxyz]/ig) ) {
 //console.log(guess);
-//var state = teamNames[random].name.split("").fill("_");
-//var random = Math.floor(Math.random() * (31 - 0) + 0);
+//const random = Math.floor(Math.random() * (31 - 0) + 0);
+//var state = teamNames[random].name.split("").fill('_');
+//var answer = teamNames[random].name;
 
         for (let i = 0; i < state.length; i++) {
 
@@ -332,42 +280,108 @@ const guess = form.guess.value;
             console.log(answer);
         }
           form.guess.value = '';
-          tries.textContent === triesCounter--;
+          ;
         }
+numTries.textContent++;
 checkWin();
+//checkLoss();
         })
+
+hint.addEventListener('click', giveHint);
+
 function checkWin() {
       if (state.includes("_") === false ) {
         document.getElementById("result-box").classList.remove("hidden");
-        document.getElementById("restart").classList.remove("hidden");
+       document.getElementById("restart").classList.remove("hidden");
       };
     }
 
+    function checkLoss() {
+      if ( numTries.textContent === 0 ) {
+        document.getElementById("lose-box").classList.remove("hidden");
+       document.getElementById("restart-lose").classList.remove("hidden");
+      }
+    }
+
+//    function restart() {
+//      var random =""
+//      var state=""
+//v
+//      var random = Math.floor(Math.random() * (31 - 0) + 0);
+//
+//      var hintbox = document.getElementById('hint-box');
+//      var numTries = document.getElementById('tries');
+//      var state = teamNames[random].name.split("").fill('_');
+//      var answer = teamNames[random].name;
+//      var triesCounter = numTries.textContent;
+
+//      var answer = teamNames[random].name;
+//      document.getElementById("result-box").classList.add("hidden");
+//      document.getElementById("lose-box").classList.add("hidden");
+//     letters.textContent = state.join(" ");
+//     hintbox.textContent = "";
+//     numTries.textContent = 0;
+
+//     function giveHint() {
+//    random = "";
+//    var random = Math.floor(Math.random() * (31 - 0) + 0);
+//
+//       var hintbox = document.getElementById('hint-box');
+//       hintbox.textContent = teamNames[random].hintOne;
+//     console.log(random);
+
+//     form.addEventListener('submit', function(event) {
+//       event.preventDefault();
+//     const guess = form.guess.value;
+
+//var random =""
+
+
+     //if (guess === includes(/[abcdefghijklmnopqrstuvwxyz]/ig) ) {
+     //console.log(guess);
+     //const random = Math.floor(Math.random() * (31 - 0) + 0);
+     //var state = teamNames[random].name.split("").fill('_');
+     //var answer = teamNames[random].name;
+
+  //         for (let i = 0; i < state.length; i++) {
+
+       //let guess = ""
+//
+  //           console.log(random);
+  //           let letter = answer[i];
+  //           if (letter === guess) {
+  //             state[i] = guess;
+  //             letters.textContent = state.join(" ");
+  //             console.log(state);
+  //             console.log(answer);
+  //         }
+  //           form.guess.value = '';
+  //           ;
+  //         }
+  //   numTries.textContent++;
+  //   checkWin();
+     //checkLoss();
+    //       })
+
+  //   hint.addEventListener('click', giveHint);
+
+  //   function checkWin() {
+  //       if (state.includes("_") === false ) {
+  //         document.getElementById("result-box").classList.remove("hidden");
+  //        document.getElementById("restart").classList.remove("hidden");
+  //       };
+  //     }
+
+  //     function checkLoss() {
+  //       if ( numTries.textContent === 0 ) {
+  //         document.getElementById("lose-box").classList.remove("hidden");
+  //        document.getElementById("restart-lose").classList.remove("hidden");
+  //       }
+  //  }
+  //   }
+
+  //  }
+
+
   replay.addEventListener('click', restartGame);
-  //a.addEventListener('click', () => takeGuess("a"));
-  //b.addEventListener('click', () => takeGuess("b"));
-  //c.addEventListener('click', () => takeGuess("c"));
-  //d.addEventListener('click', () => takeGuess("d"));
-  //e.addEventListener('click', () => takeGuess("e"));
-  //f.addEventListener('click', () => takeGuess("f"));
-  //g.addEventListener('click', () => takeGuess("g"));
-  //h.addEventListener('click', () => takeGuess("h"));
-  //i.addEventListener('click', () => takeGuess("i"));
-  //j.addEventListener('click', () => takeGuess("j"));
-  //k.addEventListener('click', () => takeGuess("k"));
-  //l.addEventListener('click', () => takeGuess("l"));
-  //m.addEventListener('click', () => takeGuess("m"));
-  //n.addEventListener('click', () => takeGuess("n"));
-  //o.addEventListener('click', () => takeGuess("o"));
-  //p.addEventListener('click', () => takeGuess("p"));
-  //q.addEventListener('click', () => takeGuess("q"));
-  //r.addEventListener('click', () => takeGuess("r"));
-  //s.addEventListener('click', () => takeGuess("s"));
-  //t.addEventListener('click', () => takeGuess("t"));
-  //u.addEventListener('click', () => takeGuess("u"));
-  //v.addEventListener('click', () => takeGuess("v"));
-  //w.addEventListener('click', () => takeGuess("w"));
-  //x.addEventListener('click', () => takeGuess("x"));
-  //y.addEventListener('click', () => takeGuess("y"));
-  //z.addEventListener('click', () => takeGuess("z"));
-  hint.addEventListener('click', giveHint);
+  //replayLose.addEventListener('click', restartGame);
